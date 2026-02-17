87e3538603b95052916261490c7df57a97073ef6
tomatorivera <tomatorivera@gmail.com>
2026-02-16 21:52:11 -0300

    Implementar i18n en la vista de login

--- src/main/java/com/tocttaviano/crudclientes/app/controllers/LoginController.java
+++ src/main/java/com/tocttaviano/crudclientes/app/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.tocttaviano.crudclientes.app.controllers;
 
 import java.security.Principal;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,28 +14,32 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class LoginController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping("/login")
 	public String login(
 			Model model, 
 			Principal principal, 
 			RedirectAttributes mensajeria,
 			@RequestParam(required = false) String error,
-			@RequestParam(required = false) String logout
+			@RequestParam(required = false) String logout,
+			Locale locale
 	){
 		// Si el usuario ya ha iniciado sesión, redirige a la página de inicio
 		if (principal != null) {
-			mensajeria.addFlashAttribute("mensajeError", "Ya has iniciado sesión anteriormente");
+			mensajeria.addFlashAttribute("mensajeError", messageSource.getMessage("Text.login.error.tieneSesion", null, locale));
 			return "redirect:/";
 		}
 		
 		// Si se ha producido un error de autenticación, muestra un mensaje de error
 		if (error != null) {
-			model.addAttribute("mensajeError", "Credenciales incorrectas. Inténtalo de nuevo.");
+			model.addAttribute("mensajeError", messageSource.getMessage("Text.login.error.credencialesIncorrectas", null, locale));
 		}
 		
 		// Si llega la bandera de logout, cierro sesión del usuario
 		if (logout != null) {
-			model.addAttribute("mensajeExito", "Has cerrado sesión correctamente.");
+			model.addAttribute("mensajeExito", messageSource.getMessage("Text.login.exito.cerrarSesion", null, locale));
 		}
 		
 		return "auth/login";
